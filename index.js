0b3dfa3360b7e7bd4fd45fdfb6f06ebbb667888d
da0shi <whit3.develop@gmail.com>
2015-05-28 18:51:52 +0900

    Request token and generate token.json
    * readline-sync を使って code を対話的に取得
    * request モジュールを使ってリクエストを POST
    * HTTP のステータスコードが 200 でないならエラーなので終了
    * body の中身を json 化してファイルに上書き保存
      * options で {json: true} を設定しているので body はオブジェクト

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 'use strict';
 var fs = require('fs');
 var querystring = require('querystring');
+var readline = require('readline-sync');
+var request = require('request');
 
 console.log("Hello Cmail");
 
@@ -26,4 +28,30 @@ if (process.argv[2] === 'auth') {
   };
   var uri = config.auth_uri +'?'+ querystring.encode(params);
   console.log(uri);
+}
+
+if (process.argv[2] === 'token') {
+  var config = JSON.parse(fs.readFileSync(',/config.json')).installed;
+  var code = readline.question('Input returned code: ');
+  var params = {
+    grant_type: 'authorization_code',
+    code: code,
+    client_id: config.client_id,
+    client_secret: config.client_secret,
+    redirect_uri: config.redirect_uris[0]
+  };
+  var options = {
+    uri: config.token_uri,
+    form: params,
+    json: true
+  };
+  request.post(options, function (error, response, body) {
+    if (response.statusCode !== 200) {
+      console.log("Error:", error);
+      console.log("Status code:", response.statusCode);
+      console.log("Body:", body);
+      return false;
+    }
+    fs.writeFileSync(',/token.json', JSON.stringify(body));
+  });
 }
