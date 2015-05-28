0e519d3232ec2b4c7bd409a15e3374216bed9257
da0shi <whit3.develop@gmail.com>
2015-05-28 18:35:59 +0900

    Generate auth request uri
    認証画面を表示させるための URI を生成，表示

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 var fs = require('fs');
+var querystring = require('querystring');
 
 console.log("Hello Cmail");
 
@@ -12,7 +13,17 @@ if (process.argv[2] === 'home') {
   console.log('HOME:', process.env.HOME);
 }
 
-if (process.argv[2] === 'init') {
+if (process.argv[2] === 'auth') {
   var config = JSON.parse(fs.readFileSync(',/config.json')).installed;
-  console.log(config);
+  var params = {
+    response_type: 'code',
+    access_type: 'offline',
+    approval_prompt: 'force',
+    client_id: config.client_id,
+    redirect_uri: config.redirect_uris[0],
+    scope: 'https://www.googleapis.com/auth/gmail.labels',
+    state: 'some random string haha'
+  };
+  var uri = config.auth_uri +'?'+ querystring.encode(params);
+  console.log(uri);
 }
